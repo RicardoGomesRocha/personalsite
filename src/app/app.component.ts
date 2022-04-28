f60d1885ba1bf3c05c7c2e2120ecae2e5ce647f9
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2022-04-28 19:18:01 -0400

    Add photo and cosumize some styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event: any) {
+    console.log(event);
+  }
+}
