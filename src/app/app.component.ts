d24387f6786d2881eca5fbe1680a122b00f685db
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2022-05-01 16:26:57 -0400

    Add angular materials to the project; add code to change photo size taking in accont the current scroll size.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,26 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  defaultVwSize = 30;
+
+  units = `vw`;
+
+  photoSize = `${this.defaultVwSize}${this.units}`;
+
+  photoWrapperSize = `100vh`;
+
+  scrollTop = 0;
+
   @HostListener('window:scroll', ['$event']) // for window scroll events
-  onScroll(event: any) {
-    console.log(event);
+  onScroll(event: Event) {
+    const target = event.target as Document;
+    this.scrollTop = target.body?.getBoundingClientRect().top ?? 0;
+    const windowSize = window.innerHeight;
+    const newSize =
+      ((windowSize + this.scrollTop) * this.defaultVwSize) / windowSize;
+    this.photoSize = `${newSize}${this.units}`;
+    const newSizeWrapper = ((windowSize + this.scrollTop) * 100) / windowSize;
+    this.photoSize = `${newSize}${this.units}`;
+    this.photoWrapperSize = `${newSizeWrapper}vh`;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialsModule } from './materials.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
