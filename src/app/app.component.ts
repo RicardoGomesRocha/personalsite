f5dbda4ef8a057e407a2f62c27cc24274b3540c3
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2022-05-04 14:48:35 -0400

    Improve background effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component, HostListener } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   defaultVwSize = 20;
 
   units = `vw`;
@@ -21,6 +27,47 @@ export class AppComponent {
   defaultPhotoMargin = this.defaultVwSize / 40;
 
   photoMargins = `${this.defaultPhotoMargin / 40}vw`;
+
+  backgroundDivs: number[] = [...new Array(250)].map((value, index) =>
+    index % 2 > 0 ? 1 : 0
+  );
+
+  @ViewChild('backgroundElement')
+  backgroundElement: ElementRef<HTMLDivElement> | undefined;
+
+  ngAfterViewInit() {
+    const elements: HTMLCollection | undefined =
+      this.backgroundElement?.nativeElement.children;
+
+    if (elements) {
+      const numberOfElements = elements.length;
+      const maxNumberVisibleElements = numberOfElements / 10;
+
+      setInterval(() => {
+        let numberVisibleElements = this.getRandomIntInclusive(
+          0,
+          numberOfElements - 1
+        );
+
+        const positionsArray = [];
+
+        for (let i = 0; i < numberOfElements; i++) {
+          elements[i].classList.remove('visible');
+          positionsArray[i] = i;
+        }
+
+        while (numberVisibleElements > 0) {
+          const randomPosition = this.getRandomIntInclusive(
+            0,
+            numberOfElements
+          );
+          const elementsPosition = positionsArray.splice(randomPosition, 1)[0];
+          elements[elementsPosition]?.classList.add('visible');
+          numberVisibleElements--;
+        }
+      }, 1000);
+    }
+  }
 
   @HostListener('window:scroll', ['$event']) // for window scroll events
   onScroll(event: Event) {
@@ -45,5 +92,11 @@ export class AppComponent {
 
   getTransformAngle(): string {
     return `rotate(${this.rotationAngle}deg)`;
+  }
+
+  getRandomIntInclusive(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
   }
 }
