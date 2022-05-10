d27ba4e9bcef1e5772b1644533436f1e989b1bed
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2022-05-10 11:21:06 -0400

    Add routing components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'blog', component: PortfolioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { IntroComponent } from './intro/intro.component';
 import { MaterialsModule } from './materials.module';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @NgModule({
-  declarations: [AppComponent, TopMenuComponent, IntroComponent],
+  declarations: [AppComponent, TopMenuComponent, IntroComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
