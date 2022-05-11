f980da27c825f9c751837c297867d1fb4b9eb1cb
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2022-05-10 22:18:42 -0400

    Add projects services to website

---
+++ src/app/models/projects.ts
@@ -0,0 +1,6 @@
+export interface Project {
+  id: string;
+  name: string;
+  backgroundImage: string;
+  description: string;
+}
---
+++ src/app/services/project.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Project } from '../models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectService {
+  getProjects(): Observable<Project[]> {
+    return of([
+      {
+        id: '1',
+        name: 'Wow! Project.',
+        backgroundImage: '',
+        description:
+          'This is some description. For fully example, I need to had some html!',
+      },
+    ]);
+  }
+}
