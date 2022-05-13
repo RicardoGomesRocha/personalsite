1b8b05bf35cafc843bf5fdfe8198999d27710ce0
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2022-05-13 15:31:38 -0400

    Improve project section.

--- src/app/materials.module.ts
+++ src/app/materials.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  exports: [MatButtonModule, MatRippleModule],
+  exports: [MatButtonModule, MatRippleModule, MatIconModule],
 })
 export class MaterialsModule {}
