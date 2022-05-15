f5770a5e1c84865fee30b500b9a92901ef2de85d
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2022-05-15 11:47:30 -0400

    Fix issues with code lint

--- src/app/blog/blog-post/blog-post.component.ts
+++ src/app/blog/blog-post/blog-post.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-blog-post',
   templateUrl: './blog-post.component.html',
-  styleUrls: ['./blog-post.component.scss']
+  styleUrls: ['./blog-post.component.scss'],
 })
-export class BlogPostComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class BlogPostComponent {}
