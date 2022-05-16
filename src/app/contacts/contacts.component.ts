38616ec2a490d189c29a0d73aef92324460ff79b
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2022-05-16 11:14:14 -0400

    Improve social media appearance

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss'],
 })
-export class ContactsComponent {}
+export class ContactsComponent {
+  subject = new FormControl('');
+  body = new FormControl('');
+  email = new FormControl('');
+}
