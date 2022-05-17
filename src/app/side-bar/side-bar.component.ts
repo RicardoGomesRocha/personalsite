758de2680a371c93386aaafdc5a4fa62962e1841
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2022-05-17 16:36:26 -0400

    Move top-menu content to side bar,

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { SideBarService } from './side-bar.service';
 
@@ -5,6 +6,28 @@ import { SideBarService } from './side-bar.service';
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.scss'],
+  animations: [
+    trigger('inOutAnimationMenu', [
+      transition(':enter', [
+        style({ 'margin-left': '-300px', opacity: 0 }),
+        animate('0.2s ease-out', style({ 'margin-left': '0px', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ 'margin-left': '0px', opacity: 1 }),
+        animate('0.2s ease-in', style({ 'margin-left': '-300px', opacity: 0 })),
+      ]),
+    ]),
+    trigger('inOutAnimationOpacity', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.2s ease-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('0.2s ease-in', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class SideBarComponent {
   $openState = this.sidebarService.$openState;
