7116c53c6174130c6fdb7b87e5ef294f5b39689b
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2022-05-27 17:44:55 -0400

    #12 Add font-size

--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { QuillModules } from 'ngx-quill';
+import Quill from 'quill';
 import 'quill-emoji/dist/quill-emoji.js';
 
 @Component({
   selector: 'app-text-editor',
   templateUrl: './text-editor.component.html',
   styleUrls: ['./text-editor.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TextEditorComponent {
   @Input()
@@ -21,23 +23,49 @@ export class TextEditorComponent {
   @Input()
   content = '';
 
-  config: QuillModules = {
-    syntax: true,
-    toolbar: {
-      container: [
-        ['bold', 'italic', 'underline'],
-        ['blockquote', 'code-block'],
-        [{ list: 'ordered' }, { list: 'bullet' }],
-        [{ script: 'sub' }, { script: 'super' }],
-        [{ size: ['small', false, 'large', 'huge'] }],
-        [{ header: [1, 2, 3, 4, 5, 6, false] }], // dropdown with defaults from theme
-        [{ align: [] }],
-        ['link'],
-        ['emoji'],
-      ],
-    },
-    'emoji-shortname': true,
-    'emoji-textarea': false,
-    'emoji-toolbar': true,
-  };
+  config: QuillModules;
+
+  constructor() {
+    const fontSizeArr = [
+      '8px',
+      '9px',
+      '10px',
+      '12px',
+      '14px',
+      '16px',
+      '20px',
+      '24px',
+      '32px',
+      '42px',
+      '54px',
+      '68px',
+      '84px',
+      '98px',
+    ];
+
+    const Size = Quill.import('attributors/style/size');
+    Size.whitelist = fontSizeArr;
+    Quill.register(Size, true);
+
+    this.config = {
+      syntax: true,
+      toolbar: {
+        container: [
+          ['bold', 'italic', 'underline'],
+          ['blockquote', 'code-block'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          [{ script: 'sub' }, { script: 'super' }],
+          [{ size: fontSizeArr }],
+          [{ header: [1, 2, 3, 4, 5, 6, false] }], // dropdown with defaults from theme
+          [{ align: [] }],
+          ['link'],
+          ['emoji'],
+          ['clean'],
+        ],
+      },
+      'emoji-shortname': true,
+      'emoji-textarea': false,
+      'emoji-toolbar': true,
+    };
+  }
 }
